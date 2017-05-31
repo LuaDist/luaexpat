3a738c95eb966531e58fe24c0673e760ee99a426
silverwind <me@silverwind.io>
2017-05-31 17:18:50 +0200

    fix compatibilty with LuaJIT 2.1

--- src/lxplib.c
+++ src/lxplib.c
@@ -586,7 +586,7 @@ static void set_info (lua_State *L) {
 }
 
 
-#if !defined LUA_VERSION_NUM || LUA_VERSION_NUM==501
+#if !defined(luaL_newlibtable) && (!defined LUA_VERSION_NUM || LUA_VERSION_NUM==501)
 /*
 ** Adapted from Lua 5.2.0
 */
